e157d6eb158b0586a70105e4ba0e4c10188df3a4
Kwanghoon Choi <lazyswamp@gmail.com>
2020-02-27 14:43:11 +0900

    Putting the AST type behind the Expr type.

--- app/polyrpc/ast/Expr.hs
+++ app/polyrpc/ast/Expr.hs
@@ -3,62 +3,6 @@ module Expr where
 import Type
 
 --
-data AST =
-    ASTExprSeq { fromASTExprSeq :: [Expr] }
-  | ASTExpr    { fromASTExpr    :: Expr   }
-  | ASTIdSeq   { fromASTIdSeq   :: [String] }
-  | ASTId      { fromASTId      :: String }
-  | ASTTypeSeq { fromASTTypeSeq :: [Type] }
-  | ASTType    { fromASTType    :: Type  }
-  | ASTLocationSeq { fromASTLocationSeq :: [Location] }
-  | ASTLocation    { fromASTLocation    :: Location  }
-  
-  | ASTBindingDeclSeq { fromASTBindingDeclSeq :: [BindingDecl] }
-  | ASTBindingDecl    { fromASTBindingDecl    :: BindingDecl  }
-
-  | ASTDataTypeDecl { fromASTDataTypeDecl :: DataTypeDecl }
-
-  | ASTTopLevelDeclSeq { fromASTTopLevelDeclSeq :: [TopLevelDecl] }
-  
-  | ASTTypeConDeclSeq { fromASTTypeConDeclSeq :: [TypeConDecl] }
-  | ASTTypeConDecl { fromASTTypeConDecl :: TypeConDecl }
-  
-  | ASTIdLocSeq { fromASTIdLocSeq :: [(String,Location)] }
-  | ASTIdLoc { fromASTIdLoc :: (String,Location) }
-  
-  | ASTAlternativeSeq { fromASTAlternativeSeq :: [Alternative] }
-  | ASTAlternative { fromASTAlternative :: Alternative }
-  
-  | ASTLit { fromASTLit :: Lit }
-
-toASTExprSeq exprs = ASTExprSeq exprs
-toASTExpr expr     = ASTExpr expr
-toASTIdSeq   ids   = ASTIdSeq ids
-toASTId   id       = ASTId id
-toASTTypeSeq types = ASTTypeSeq types
-toASTType ty     = ASTType ty
-toASTLocationSeq locations = ASTLocationSeq locations
-toASTLocation location     = ASTLocation location
-
-toASTBindingDeclSeq bindings = ASTBindingDeclSeq bindings
-toASTBindingDecl binding     = ASTBindingDecl binding
-
-toASTDataTypeDecl datatype     = ASTDataTypeDecl datatype
-
-toASTTopLevelDeclSeq toplevel = ASTTopLevelDeclSeq toplevel
-
-toASTTypeConDeclSeq typecondecls = ASTTypeConDeclSeq typecondecls
-toASTTypeConDecl typecondecl     = ASTTypeConDecl typecondecl
-
-toASTIdLocSeq idlocs = ASTIdLocSeq idlocs
-toASTIdLoc idloc     = ASTIdLoc idloc
-
-toASTAlternativeSeq alts = ASTAlternativeSeq alts
-toASTAlternative alt     = ASTAlternative alt
-
-toASTLit lit     = ASTLit lit
-
---
 data Expr =
     Var String
   | TypeAbs [String] Expr
@@ -114,3 +58,61 @@ data TypeConDecl =
 
 data Alternative =
   Alternative String [String] Expr
+
+
+--
+data AST =
+    ASTExprSeq { fromASTExprSeq :: [Expr] }
+  | ASTExpr    { fromASTExpr    :: Expr   }
+  | ASTIdSeq   { fromASTIdSeq   :: [String] }
+  | ASTId      { fromASTId      :: String }
+  | ASTTypeSeq { fromASTTypeSeq :: [Type] }
+  | ASTType    { fromASTType    :: Type  }
+  | ASTLocationSeq { fromASTLocationSeq :: [Location] }
+  | ASTLocation    { fromASTLocation    :: Location  }
+  
+  | ASTBindingDeclSeq { fromASTBindingDeclSeq :: [BindingDecl] }
+  | ASTBindingDecl    { fromASTBindingDecl    :: BindingDecl  }
+
+  | ASTDataTypeDecl { fromASTDataTypeDecl :: DataTypeDecl }
+
+  | ASTTopLevelDeclSeq { fromASTTopLevelDeclSeq :: [TopLevelDecl] }
+  
+  | ASTTypeConDeclSeq { fromASTTypeConDeclSeq :: [TypeConDecl] }
+  | ASTTypeConDecl { fromASTTypeConDecl :: TypeConDecl }
+  
+  | ASTIdLocSeq { fromASTIdLocSeq :: [(String,Location)] }
+  | ASTIdLoc { fromASTIdLoc :: (String,Location) }
+  
+  | ASTAlternativeSeq { fromASTAlternativeSeq :: [Alternative] }
+  | ASTAlternative { fromASTAlternative :: Alternative }
+  
+  | ASTLit { fromASTLit :: Lit }
+
+toASTExprSeq exprs = ASTExprSeq exprs
+toASTExpr expr     = ASTExpr expr
+toASTIdSeq   ids   = ASTIdSeq ids
+toASTId   id       = ASTId id
+toASTTypeSeq types = ASTTypeSeq types
+toASTType ty     = ASTType ty
+toASTLocationSeq locations = ASTLocationSeq locations
+toASTLocation location     = ASTLocation location
+
+toASTBindingDeclSeq bindings = ASTBindingDeclSeq bindings
+toASTBindingDecl binding     = ASTBindingDecl binding
+
+toASTDataTypeDecl datatype     = ASTDataTypeDecl datatype
+
+toASTTopLevelDeclSeq toplevel = ASTTopLevelDeclSeq toplevel
+
+toASTTypeConDeclSeq typecondecls = ASTTypeConDeclSeq typecondecls
+toASTTypeConDecl typecondecl     = ASTTypeConDecl typecondecl
+
+toASTIdLocSeq idlocs = ASTIdLocSeq idlocs
+toASTIdLoc idloc     = ASTIdLoc idloc
+
+toASTAlternativeSeq alts = ASTAlternativeSeq alts
+toASTAlternative alt     = ASTAlternative alt
+
+toASTLit lit     = ASTLit lit
+
