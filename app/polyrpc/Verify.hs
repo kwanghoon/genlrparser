4010dd2fdb230ddd21e0c97d48a4b06c4dcd7a6c
Kwanghoon Choi <lazyswamp@gmail.com>
2020-04-03 15:00:41 +0900

    Todo: Verify CS programs and execute them

---
+++ app/polyrpc/Verify.hs
@@ -0,0 +1,13 @@
+module Verify where
+
+import Location
+import CSType
+import CSExpr
+
+
+verify :: Monad m => GlobalTypeInfo -> FunctionStore -> [TopLevelDecl] -> m ()
+verify gti funStore toplevelDecls = do
+  return ()
+
+
+
